8547981a7e45cccec9a406b0a875791fd3f49be6
l3on <l3onwebdev@gmail.com>
2024-02-04 21:58:37 +0100

    added doctors fetch

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,55 @@
 <script setup>
+import { ref } from 'vue';
+import { apiClient } from '@/http';
+
+
+//*** COMPONENTS ***//
+import DoctorCard from '@/components/doctors/DoctorCard.vue';
+
+
+//*** DOCTORS ***//
+const doctors = ref([]);
+const isLoading = ref(false);
+
+const fetchDoctors = () => {
+    isLoading.value = true;
+
+    apiClient.get('/doctors')
+        .then(({ data }) => {
+            doctors.value = data;
+        })
+        .catch(err => {
+            console.error(err);
+        })
+        .then(() => {
+            isLoading.value = false;
+        });
+}
+
+fetchDoctors();
 
 </script>
 
 
 <template>
     <section class="container-fluid">
-        <h1>Hello World</h1>
+
+        <div v-if="!isLoading">
+
+            <!-- Section Title -->
+            <header>
+                <h2>Dottori in evidenza</h2>
+            </header>
+
+            <!-- Doctors -->
+            <ul v-if="doctors.length" class="row row-cols-2">
+                <li v-for="doctor in doctors" :key="doctor.id" class="col">
+                    <DoctorCard :doctor="doctor" />
+                </li>
+            </ul>
+            <div v-else>Non ci sono risultati.</div>
+        </div>
+
     </section>
 </template>
 
