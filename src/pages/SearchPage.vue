21cf183b63327c86a71d6579d35667c919a1a966
l3on <l3onwebdev@gmail.com>
2024-02-11 20:08:54 +0100

    added base search page

---
+++ src/pages/SearchPage.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { computed, ref, watch } from 'vue';
+import { useRoute } from 'vue-router';
+import { apiClient } from '@/http';
+
+
+//*** COMPONENTS ***//
+import AppLoader from '@/components/AppLoader.vue';
+import DoctorCard from '@/components/doctors/DoctorCard.vue';
+
+
+//*** DOCTORS ***//
+const doctors = ref([]);
+const isLoading = ref(false);
+const route = useRoute();
+
+const fetchDoctors = () => {
+
+    const params = { ...route.query };
+    isLoading.value = true;
+
+    apiClient.get('/doctors/search', { params })
+        .then(({ data }) => {
+            doctors.value = data;
+        })
+        .catch(err => {
+            console.error(err);
+        })
+        .then(() => {
+            isLoading.value = false;
+        });
+}
+
+
+// Watch query cahnges
+const routeQuery = computed(() => route.query);
+
+watch(routeQuery, () => {
+    fetchDoctors();
+}, { immediate: true });
+
+</script>
+
+
+<template>
+    <section class="container-fluid">
+
+        <div v-if="!isLoading">
+
+            <!-- Section Title -->
+            <header class="mb-3">
+                <h3>Risultati:</h3>
+            </header>
+
+            <!-- Doctors -->
+            <ul v-if="doctors.length" class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                <li v-for="doctor in doctors" :key="doctor.id" class="col mb-3">
+                    <DoctorCard :doctor="doctor" />
+                </li>
+            </ul>
+            <div v-else>Non ci sono risultati.</div>
+        </div>
+
+        <AppLoader v-if="isLoading" />
+    </section>
+</template>
+
+
+<style scoped></style>
